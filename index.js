ac5fb679170687871be0b8dd1ffd1987dcbc29df
Brindha S <brinsree@gmail.com>
2018-04-18 21:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,15 +62,14 @@ function viewCart()
 
 function removeFromCart(item) {
   // write your code here
-  for(var i=0;i<cart.length;i++){
+    for(var i=0;i<cart.length;i++){
     if(cart[i].itemName===item){
        cart.splice(i,1);
-      return cart;
-      } else{
-        return "That item is not in your cart";
-      }
-  }
-}
+       return cart;
+      } 
+     }
+      return "That item is not in your cart";
+   }
 
 function placeOrder(cardNumber) {
   // write your code here
