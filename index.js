5d8a8e099fbd3e26cbb6dac1d8af5015185416fa
Brindha S <brinsree@gmail.com>
2018-04-18 21:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {return  "Sorry, we don't have a credit card on file for you.";} else{
     var bill=total();
     cart=[];
-    return `Your total cost is ${bill}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is \$${bill}, which will be charged to the card ${cardNumber}.`;
   }
   
 }
